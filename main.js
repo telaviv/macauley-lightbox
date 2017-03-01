d409d1794c5f5f6f4306cb9e752f087284681a3d
Shawn Krisman <krisman.shawn@gmail.com>
2017-02-28 23:45:50 -0800

    Wrote GiphyMediaState.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const fetchJSON = (url) => {
   return new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
@@ -16,7 +18,7 @@ const fetchJSON = (url) => {
 };
 
 const createGiphyURL = (query) => {
-  const base =  'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+  const base =  'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&&limit=10&q=';
   const queryParam = query.replace(' ', '+');
   return base + queryParam;
 }
@@ -29,3 +31,43 @@ const fetchGiphyData = (query) => {
     }));
   });
 };
+
+class GiphyMediaState {
+  /* An array of giphy data that async loads over time.
+
+     Displaying a gif in this lightbox design takes two pieces of information
+
+     1) The actual mp4 video to display.
+     2) The color to display outside of the video. Derived from a still of the video.
+
+     All in all we will need to load 10 videos and 10 images to derive our color. To
+     make this performant we want to load all these images and videos simultaneously.
+
+     GiphyMediaState is a helper class that encapsulates the complexity of a data
+     structure that is loading over time. At the heart of the data structure is an
+     array of objects with the following properties:
+
+         videoElement - The video element of the gif.
+         color - A string that can be a css property value.
+
+     When either of these values are undefined, then the data is not loaded yet.
+  */
+  constructor() {
+    this.state = [];
+    // note: make 10 a constant;
+    for (let i = 0; i < 10; ++i) {
+      this.state.push({});
+    }
+  }
+
+  load(query) {
+    // Load the giphy query into the GiphyMediaState
+    //
+    // It's a bit odd to have what is essentially the second part of the constructor be a
+    // second method. It's also weird to have a constructor with a side effect of making
+    // 20 requests.
+
+    fetchGiphyData(query);
+    return this;
+  }
+}
